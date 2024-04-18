e511cc7db65353590f4da26c7f0099b3ab01fec0
Sakhawath Hossen Sohan <11808582+localhost443@users.noreply.github.com>
2024-04-18 10:31:31 +0600

    added validator for coffee DTO

--- src/coffees/dto/coffee.dto.ts
+++ src/coffees/dto/coffee.dto.ts
@@ -1,5 +1,12 @@
+import { IsString } from 'class-validator';
+
 export class CoffeeDto {
+  @IsString()
   name: string;
+
+  @IsString()
   brand: string;
+
+  @IsString({ each: true })
   flavors: string[];
 }
