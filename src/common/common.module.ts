47acb52d3eaa5bb67b423833d13c83978bb928f4
Sakhawath Hossen Sohan <11808582+localhost443@users.noreply.github.com>
2024-04-25 12:40:41 +0600

    added interceptor for timeout error

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
-import { APP_GUARD } from '@nestjs/core';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { ApiKeyGuard } from './guards/api-key.guard';
+import { WrapResponseInterceptor } from './interceptor/wrap-response.interceptor';
+import { TimeoutInterceptorInterceptor } from './interceptor/timeout-interceptor.interceptor';
 
 @Module({
-  providers: [{ provide: APP_GUARD, useClass: ApiKeyGuard }],
+  providers: [
+    { provide: APP_GUARD, useClass: ApiKeyGuard },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: WrapResponseInterceptor,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: TimeoutInterceptorInterceptor,
+    },
+  ],
 })
 export class CommonModule {}
